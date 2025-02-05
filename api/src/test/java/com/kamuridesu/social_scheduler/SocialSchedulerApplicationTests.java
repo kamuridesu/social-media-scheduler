edd920c235c981a76d15e3ec1937d612a036a9e1
Kamuri Amorim <myk.gata14@gmail.com>
2025-02-04 23:05:20 -0300

    initial commit

---
+++ api/src/test/java/com/kamuridesu/social_scheduler/SocialSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamuridesu.social_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
