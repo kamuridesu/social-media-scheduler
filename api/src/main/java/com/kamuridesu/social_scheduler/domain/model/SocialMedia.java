c93769b039f126db3d1d7ff776ee21524e19f059
Kamuri Amorim <myk.gata14@gmail.com>
2025-02-10 15:01:49 -0300

    feat: relationship

--- api/src/main/java/com/kamuridesu/social_scheduler/domain/model/SocialMedia.java
+++ api/src/main/java/com/kamuridesu/social_scheduler/domain/model/SocialMedia.java
@@ -1,18 +1,21 @@
 package com.kamuridesu.social_scheduler.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Post {
-
+public class SocialMedia {
     @Id
     @GeneratedValue
     Long id;
 
-    String body;
-    Integer delay;
-    Integer creation_timestamp;
+    @Column(unique = true)
+    String name;
 
+    String token;
+    String expiresAt;
+    String refreshToken;
+    String refreshExpiresAt;
 }
--- api/src/main/java/com/kamuridesu/social_scheduler/domain/model/User.java
+++ api/src/main/java/com/kamuridesu/social_scheduler/domain/model/User.java
@@ -1,8 +1,13 @@
 package com.kamuridesu.social_scheduler.domain.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,6 +17,16 @@ public class User {
     @Id
     @GeneratedValue
     Long id;
+    
+    @Column(unique = true)
     String username;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    List<SocialMedia> socialMedia;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    List<Post> posts;
     
 }
